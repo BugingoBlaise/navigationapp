66703fb4038ca3e80cfe213144b7d3fce8efbe00
BugingoBlaise <blaisemugisha1@gmail.com>
2024-02-12 14:28:29 +0200

    nice and okay

---
+++ lib/main.dart
@@ -0,0 +1,313 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:math' as math;
+
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Navigation Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+
+  // List of screens to be displayed in each tab
+  final List<Widget> _tabs = [
+    Image.asset(
+      'assets/onee.jpg',
+      fit: BoxFit.cover,
+    ),
+    Calculator(),
+    Image.asset(
+      'assets/background.jpg',
+      fit: BoxFit.cover,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student App'),
+      ),
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calculate),
+            label: 'Calculator',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Student',
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              decoration: BoxDecoration(
+                color: Color(0xFF1d2630),
+              ),
+              child: Text(
+                'Navigation Drawer',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Icon(Icons.account_circle),
+              title: Text('My Account'),
+              onTap: () {
+                // Handle navigation to the profile screen
+
+                Navigator.pop(context);
+                // Implement your navigation logic here
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.settings),
+              title: Text('Settings'),
+              onTap: () {
+                // Handle navigation to the settings screen
+                Navigator.pop(context);
+                // Implement your navigation logic here
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TabScreen extends StatelessWidget {
+  final String title;
+
+  TabScreen({required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        title,
+        style: TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
+
+class Calculator extends StatefulWidget {
+  @override
+  State<Calculator> createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  String userInput = "";
+  String result = "0";
+
+  List<String> buttonString = [
+    'AC',
+    '(',
+    ')',
+    '/',
+    '7',
+    '8',
+    '9',
+    '*',
+    '4',
+    '5',
+    '6',
+    '+',
+    '1',
+    '2',
+    '3',
+    '-',
+    'C',
+    '0',
+    '.',
+    '='
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1d2630),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.centerRight,
+            child: Text(
+              userInput,
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(10),
+            alignment: Alignment.centerRight,
+            child: Text(
+              result,
+              style: TextStyle(
+                fontSize: 38,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(height: 50), // Add some space between result and buttons
+          Divider(color: Colors.white),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(10),
+              child: GridView.builder(
+                itemCount: buttonString.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                  crossAxisSpacing: 12,
+                  mainAxisSpacing: 12,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return CustomButton(buttonString[index]);
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget CustomButton(String text) {
+    return InkWell(
+      splashColor: Color(0xFF1d2630),
+      onTap: () {
+        setState(() {
+          handleButtons(text);
+        });
+      },
+      child: Ink(
+        decoration: BoxDecoration(
+          color: getBgColor(text),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.white.withOpacity(0.1),
+              blurRadius: 4,
+              spreadRadius: 0.5,
+              offset: Offset(-3, -3),
+            ),
+          ],
+        ),
+        child: Center(
+          child: Text(
+            text,
+            style: TextStyle(
+              color: getColor(text),
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  getColor(String text) {
+    if (text == '/' ||
+        text == '*' ||
+        text == '+' ||
+        text == '-' ||
+        text == '(' ||
+        text == ')') {
+      return Color.fromARGB(255, 252, 100, 100);
+    }
+    return Colors.white;
+  }
+
+  getBgColor(String text) {
+    if (text == 'AC') {
+      return Color.fromARGB(255, 252, 100, 100);
+    }
+    if (text == '=') {
+      return Color.fromARGB(255, 104, 104, 159);
+    }
+    return Color(0xFF1d2630);
+  }
+
+  handleButtons(String text) {
+    if (text == 'AC') {
+      userInput = '';
+      result = '0';
+      return;
+    }
+    if (text == 'C') {
+      if (userInput.isNotEmpty) {
+        userInput = userInput.substring(0, userInput.length - 1);
+      } else {
+        return null;
+      }
+    }
+
+    if (text == '=') {
+      result = calculate();
+      // userInput = result;
+
+      if (userInput.endsWith('.0')) {
+        userInput = userInput.replaceAll('.0', '');
+      }
+      if (result.endsWith('.0')) {
+        result = result.replaceAll('.0', '');
+        return;
+      }
+    }
+
+    userInput = userInput + text;
+  }
+
+  String calculate() {
+    try {
+      var exp = Parser().parse(userInput);
+      var evaluation = exp.evaluate(EvaluationType.REAL, ContextModel());
+      return evaluation.toString();
+    } catch (e) {
+      return 'Error';
+    }
+  }
+}
